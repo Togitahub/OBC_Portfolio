27d8b67c437c6218b869463b80545158acf04d86
Your Name <totmom07@gmail.com>
2025-10-23 23:30:35 -0600

    Bugs Fixed

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,5 +1,6 @@
-import { IoCarSportSharp, IoBook } from "react-icons/io5";
+import catalog_cover from "/assets/images/catalog/catalog_cover.png";
 
+import { IoCarSportSharp, IoBook } from "react-icons/io5";
 import {
 	SiJavascript,
 	SiMongodb,
@@ -25,11 +26,15 @@ export const projectsData = [
 				description:
 					"Full-stack vehicle inventory management system built with Node.js, Express, GraphQL (Apollo Server), MongoDB, and React. Features a public catalog with advanced search and filtering capabilities, alongside a comprehensive admin panel for CRUD operations on vehicles, brands, and models. Implements JWT authentication, Cloudinary integration for image management, and real-time inventory statistics. This scalable solution streamlines dealership operations while providing customers with an intuitive, responsive interface for browsing available vehicles.",
 			},
-			es: {
+			"es-419": {
 				name: "Catalogo de Venta de Vehículos",
 				description:
 					"Sistema full-stack de gestión de inventario de vehículos construido con Node.js, Express, GraphQL (Apollo Server), MongoDB y React. Incluye un catálogo público con funciones avanzadas de búsqueda y filtrado, junto con un panel de administración completo para operaciones CRUD sobre vehículos, marcas y modelos. Implementa autenticación JWT, integración con Cloudinary para la gestión de imágenes y estadísticas de inventario en tiempo real. Esta solución escalable optimiza las operaciones del concesionario y ofrece a los clientes una interfaz intuitiva y responsive para explorar los vehículos disponibles.",
 			},
+		},
+		link: "https://importacionesmbv.com/",
+		images: {
+			cover: catalog_cover,
 		},
 		icon: IoCarSportSharp,
 		techs: [
@@ -51,7 +56,7 @@ export const projectsData = [
 				description:
 					"Full-stack application developed using Python (Django) and JavaScript (React) to create a platform for managing and viewing educational digital resources. 🖥️ It allows users to organize, search, and access a virtual collection of books and documents in an intuitive and efficient manner. This robust system enhances the online learning experience and bibliographic content management.",
 			},
-			es: {
+			"es-419": {
 				name: "Biblioteca Virtual",
 				description:
 					"Aplicación full-stack desarrollada con Python (Django) y JavaScript (React) para crear una plataforma para gestionar y visualizar recursos educativos digitales. Permite a los usuarios organizar, buscar y acceder a una colección virtual de libros y documentos de forma intuitiva y eficiente. Este sistema robusto mejora la experiencia de aprendizaje en línea y la gestión de contenidos bibliográficos.",
--- src/views/ProjectView.jsx
+++ src/views/ProjectView.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import { projectsData } from "../data/projectsData";
+
+import { useTranslation } from "react-i18next";
+import Button from "../components/common/Button";
 
 const ProjectView = () => {
-  return (
-    <div>ProjectView</div>
-  )
-}
+	const { id } = useParams();
+	const intId = Number(id);
 
-export default ProjectView
+	const { t, i18n } = useTranslation();
+	const currentLang = i18n.language;
+
+	const project = projectsData.find((p) => p.id === intId);
+
+	return (
+		<div className="relative z-10 flex flex-col gap-5 items-center py-10 px-5">
+			<h1 className="text-5xl text-first border-2 rounded-2xl px-3 py-4">
+				{project.translations[currentLang].name}
+			</h1>
+			<div className="w-full text-justify md:text-lg text-first md:font-medium bg-main border-2 rounded-2xl p-4 mt-4">
+				{project.translations[currentLang].description}
+			</div>
+			<div className="flex flex-col justify-center gap-3 text-first">
+				<h2 className="text-3xl text-center">{t("technologies")}</h2>
+				<div className="flex justify-center gap-3">
+					{project.techs.map((Tech, idx) => (
+						<Tech key={idx} className="size-8" />
+					))}
+				</div>
+			</div>
+			<div className="mt-10 animate-floating">
+				<Button
+					variation={"anchor"}
+					link={project.link}
+					text={t("buttons.project_site")}
+					isRoundedFull={true}
+				/>
+			</div>
+			<div className="w-full opacity-75">
+				<img
+					src={project.images.cover}
+					alt="catalog_cover"
+					className="rounded-2xl"
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default ProjectView;
